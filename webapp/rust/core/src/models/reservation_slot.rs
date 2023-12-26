04b82b2bf9ac0eaf00875e262cb25921febc960f
Keiji Yoshimi <walf443@gmail.com>
2023-12-26 23:38:21 +0900

    use ReservationSlotId

--- webapp/rust/core/src/models/reservation_slot.rs
+++ webapp/rust/core/src/models/reservation_slot.rs
@@ -1,8 +1,12 @@
+use crate::models::id::Id;
+
 #[derive(Debug, sqlx::FromRow)]
 pub struct ReservationSlot {
     #[allow(unused)]
-    pub id: i64,
+    pub id: Id<Self>,
     pub slot: i64,
     pub start_at: i64,
     pub end_at: i64,
 }
+
+pub type ReservationSlotId = Id<ReservationSlot>;
