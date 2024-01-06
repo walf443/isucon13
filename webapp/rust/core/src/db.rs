b2620b166a3ee84a32186d621211aba78c450f9f
Keiji Yoshimi <walf443@gmail.com>
2024-01-06 11:50:29 +0900

    move build_mysql_options to core

--- webapp/rust/core/src/db.rs
+++ webapp/rust/core/src/db.rs
@@ -6,3 +6,30 @@ pub type DBConn = MySqlConnection;
 pub trait HaveDBPool {
     fn get_db_pool(&self) -> &DBPool;
 }
+pub fn build_mysql_options() -> sqlx::mysql::MySqlConnectOptions {
+    let mut options = sqlx::mysql::MySqlConnectOptions::new()
+        .host("127.0.0.1")
+        .port(3306)
+        .username("isucon")
+        .password("isucon")
+        .database("isupipe");
+    if let Ok(host) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_ADDRESS") {
+        options = options.host(&host);
+    }
+    if let Some(port) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_PORT")
+        .ok()
+        .and_then(|port_str| port_str.parse().ok())
+    {
+        options = options.port(port);
+    }
+    if let Ok(user) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_USER") {
+        options = options.username(&user);
+    }
+    if let Ok(password) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_PASSWORD") {
+        options = options.password(&password);
+    }
+    if let Ok(database) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_DATABASE") {
+        options = options.database(&database);
+    }
+    options
+}
--- webapp/rust/http-app/src/main.rs
+++ webapp/rust/http-app/src/main.rs
@@ -2,34 +2,7 @@ use isupipe_http_core::routes::routes;
 use isupipe_http_core::state::AppState;
 use isupipe_infra::services::manager::ServiceManagerInfra;
 use std::sync::Arc;
-
-fn build_mysql_options() -> sqlx::mysql::MySqlConnectOptions {
-    let mut options = sqlx::mysql::MySqlConnectOptions::new()
-        .host("127.0.0.1")
-        .port(3306)
-        .username("isucon")
-        .password("isucon")
-        .database("isupipe");
-    if let Ok(host) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_ADDRESS") {
-        options = options.host(&host);
-    }
-    if let Some(port) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_PORT")
-        .ok()
-        .and_then(|port_str| port_str.parse().ok())
-    {
-        options = options.port(port);
-    }
-    if let Ok(user) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_USER") {
-        options = options.username(&user);
-    }
-    if let Ok(password) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_PASSWORD") {
-        options = options.password(&password);
-    }
-    if let Ok(database) = std::env::var("ISUCON13_MYSQL_DIALCONFIG_DATABASE") {
-        options = options.database(&database);
-    }
-    options
-}
+use isupipe_core::db::build_mysql_options;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
